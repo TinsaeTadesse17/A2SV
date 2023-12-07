a222a886cd326ea07794cee8b24065a5a87387cb
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2023-12-07 19:10:36 +0300

    Added solution for Adding Spaces to a String

---
+++ Leetcode_Solutions/adding-spaces-to-a-string.py
@@ -0,0 +1,10 @@
+class Solution:
+    def addSpaces(self, s: str, spaces: List[int]) -> str:
+        words = []
+        words.append(s[:spaces[0]])
+        for i in range(1,len(spaces)):
+          words.append(s[spaces[i-1]:spaces[i]])
+        words.append(s[spaces[len(spaces)-1]:])
+        return ' '.join(words)
+
+            
