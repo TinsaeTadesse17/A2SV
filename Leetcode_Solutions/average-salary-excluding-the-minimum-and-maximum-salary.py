b94a5dfe8aad2b86970438c6a7322fdf2b64350d
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2023-12-08 13:19:30 +0300

    Added solution for Average Salary Excluding the Minimum and Maximum Salary

--- Leetcode_Solutions/average-salary-excluding-the-minimum-and-maximum-salary.py
+++ Leetcode_Solutions/average-salary-excluding-the-minimum-and-maximum-salary.py
@@ -1,24 +1,6 @@
 class Solution:
     def average(self, salary: List[int]) -> float:
-        sum = 0
-        count = 0
-        avg = 0
-        maxsalary = float("-inf")
-        minsalary = float("inf")
-        for i in salary:
-            maxsalary = max(maxsalary, i)
-            minsalary = min(minsalary, i)
-
-        for j in salary:
-            if j == maxsalary or j == minsalary:
-                continue
-            sum += j
-            count += 1
-        avg = sum / count
-        return avg
-        
-
-
-
-
-        
+        salary.sort()
+        salary.remove(salary[0])
+        salary.remove(salary[len(salary)-1])
+        return sum(salary)/len(salary)
