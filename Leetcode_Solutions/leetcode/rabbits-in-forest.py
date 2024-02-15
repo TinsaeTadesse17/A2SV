bd5a2301d70220547b0974bd5f15e1aede0c21b3
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-02-15 13:53:48 +0300

    Add solution for Rabbits in Forest

--- Leetcode_Solutions/leetcode/rabbits-in-forest.py
+++ Leetcode_Solutions/leetcode/rabbits-in-forest.py
@@ -1,16 +1,9 @@
 class Solution:
     def numRabbits(self, answers: List[int]) -> int:
         counter = Counter(answers)
-        ans = counter[0]
+        ans = 0
         nums = list(set(answers))
-
-        if 0 in nums:
-            nums = nums[1:]
- 
         for i in nums:
-            if i == 1:
-                ans += counter[1] + (counter[1] % 2)
-            else:
-                ans += (i+1) * ceil(counter[i]/(i+1))
+            ans += (i+1) * ceil(counter[i]/(i+1))
                 
         return ans
