8db648b633f1e04bfe29652caac2291a3fbb09af
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-02-18 21:04:15 +0300

    Add solution for Longest Palindrome

--- Leetcode_Solutions/leetcode/longest-palindrome.py
+++ Leetcode_Solutions/leetcode/longest-palindrome.py
@@ -1,10 +1,13 @@
 class Solution:
-  def longestPalindrome(self, s: str) -> int:
-    ans = 0
-    count = collections.Counter(s)
-
-    for letter in count.values():
-      ans += letter if letter % 2 == 0 else letter - 1
-
-    oddCount = any(letter % 2 == 1 for letter in count.values())
-    return ans + oddCount
+    def longestPalindrome(self, s: str) -> int:
+        n = len(s)
+        ans = 0
+        oddCount = 0
+        counter = Counter(s)
+        for val in counter.values():
+            if val % 2 == 0:
+                ans += val
+            else:
+                ans += val - 1
+        oddCount = any(x % 2 == 1 for x in counter.values())
+        return ans + oddCount
