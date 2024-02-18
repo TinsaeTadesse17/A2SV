928ce8fe1546af6cb8ce504b1c12124a3dff1abd
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-02-19 01:33:01 +0300

    Add solution for Valid Parentheses

--- Leetcode_Solutions/leetcode/valid-parentheses.py
+++ Leetcode_Solutions/leetcode/valid-parentheses.py
@@ -1,15 +1,41 @@
+class Node:
+    def __init__(self, data):
+        self.val = data
+        self.next = None
+
+class Stack:
+    def __init__(self):
+        self.head = None
+
+    def isEmpty(self):
+        return self.head is None
+
+    def push(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def pop(self):
+        if self.isEmpty():
+            return None
+        else:
+            temp = self.head
+            self.head = self.head.next
+            return temp.val 
+
 class Solution:
-  def isValid(self, s: str) -> bool:
-    stack = []
-
-    for c in s:
-      if c == '(':
-        stack.append(')')
-      elif c == '{':
-        stack.append('}')
-      elif c == '[':
-        stack.append(']')
-      elif not stack or stack.pop() != c:
-        return False
-
-    return not stack
+    def isValid(self, s: str) -> bool:
+        stack = Stack()
+        brackets_map = {')': '(', '}': '{', ']': '['}
+        
+        for char in s:
+            if char in ['(', '{', '[']:
+                stack.push(char)
+            elif char in [')', '}', ']']:
+                if stack.isEmpty():
+                    return False
+                top_char = stack.pop()
+                if brackets_map[char] != top_char:
+                    return False
+        
+        return stack.isEmpty()
