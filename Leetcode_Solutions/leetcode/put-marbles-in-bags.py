296fa82449eb76a32a94559e7c41286c315fccaf
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-02-19 02:17:53 +0300

    Add solution for Put Marbles in Bags

--- Leetcode_Solutions/leetcode/put-marbles-in-bags.py
+++ Leetcode_Solutions/leetcode/put-marbles-in-bags.py
@@ -1,5 +1,18 @@
 class Solution:
-  def putMarbles(self, weights: List[int], k: int) -> int:
+    def putMarbles(self, weights: List[int], k: int) -> int:
+        # create an array of weights.length-1
+        pairCosts = [0] * (len(weights)-1)
+        
+        # store all the possible Pairs sum
+        for i in range(len(weights) - 1):
+            pairCosts[i] = weights[i] + weights[i + 1]
 
-    A = [a + b for a, b in itertools.pairwise(weights)]
-    return sum(heapq.nlargest(k - 1, A)) - sum(heapq.nsmallest(k - 1, A))
+        # ascending order sorting
+        pairCosts.sort()
+
+        ans = 0
+        # get the sum for all pairs differences
+        for i in range(k - 1):
+            ans += pairCosts[len(pairCosts) - 1 - i] - pairCosts[i]
+
+        return ans
