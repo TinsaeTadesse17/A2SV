c4ab33720c7bc8e07fdfc2813c5f901249e1e5d0
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-08-28 10:25:15 +0300

    Created 'Fraction Addition and Subtraction' by A2SV Hub

---
+++ 28-Aug-2024/Fraction Addition and Subtraction 208559.py
@@ -0,0 +1,20 @@
+# Problem: Fraction Addition and Subtraction - https://leetcode.com/problems/fraction-addition-and-subtraction/
+
+from fractions import Fraction
+
+class Solution:
+    def fractionAddition(self, expression: str) -> str:
+        if expression[0] != '-':
+            expression = '+' + expression
+        result = Fraction(0, 1)
+        current_number = ""
+        for char in expression:
+            if char in '+-':
+                if current_number:
+                    result += Fraction(current_number)
+                current_number = char
+            else:
+                current_number += char
+        if current_number:
+            result += Fraction(current_number)
+        return f"{result.numerator}/{result.denominator}"
