9649cdabd26f40f2b5016edba10d756bad404093
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-08-28 12:15:03 +0300

    Time: 43 ms (74.81%), Space: 18.1 MB (83%) - LeetHub

---
+++ 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.py
@@ -0,0 +1,25 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root):
+        if not root:
+            return []
+
+        ans = []
+        stack = [root]
+
+        while stack:
+            node = stack.pop()
+            ans.append(node.val)
+            for child in node.children:
+                stack.append(child)
+
+        return ans[::-1]
+
+        
