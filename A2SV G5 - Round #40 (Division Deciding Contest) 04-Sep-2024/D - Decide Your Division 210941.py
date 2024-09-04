a48d53e4588854423f9af8d0fc22d729b00ad966
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-09-04 11:35:24 +0300

    Created 'D - Decide Your Division' by A2SV Hub

---
+++ A2SV G5 - Round #40 (Division Deciding Contest) 04-Sep-2024/D - Decide Your Division 210941.py
@@ -0,0 +1,23 @@
+# Problem: D - Decide Your Division - https://codeforces.com/gym/543431/problem/D
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    count = 0
+    if n == 1:
+        print(0)
+    else:
+        while n != 1:
+            if n % 2 == 0:
+                n = n // 2
+                count += 1
+            elif n % 3 == 0:
+                n = (2 * n) // 3
+                count += 1
+            elif n % 5 == 0:
+                n = (4 * n) // 5
+                count += 1
+            else:
+                count = -1
+                break
+        print(count)
