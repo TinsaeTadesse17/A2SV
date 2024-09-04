1c3fdaaf83f2d74f450964a427991f1d782f4fc9
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-09-04 11:34:56 +0300

    Created 'B - Diversity' by A2SV Hub

---
+++ A2SV G5 - Round #40 (Division Deciding Contest) 04-Sep-2024/B - Diversity 210939.py
@@ -0,0 +1,11 @@
+# Problem: B - Diversity - https://codeforces.com/gym/543431/problem/B
+
+string = input()
+k = int(input())
+
+if len(set(string)) >= k:
+    print(0)
+elif len(string) < k:
+    print("impossible")
+else:
+    print(k - len(set(string)))
