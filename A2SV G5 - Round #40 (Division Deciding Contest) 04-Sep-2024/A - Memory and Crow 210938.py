7da724ce5a76c058894a11e51e6ecf8549b989ce
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-09-04 11:34:52 +0300

    Created 'A - Memory and Crow' by A2SV Hub

---
+++ A2SV G5 - Round #40 (Division Deciding Contest) 04-Sep-2024/A - Memory and Crow 210938.py
@@ -0,0 +1,10 @@
+# Problem: A - Memory and Crow - https://codeforces.com/gym/543431/problem/A
+
+n = int(input())
+arr = list(map(int, input().split()))
+ans = [arr[-1]]
+
+for i in range(n-2,-1,-1):
+    ans.append(arr[i+1] + arr[i])
+
+print(*ans[::-1])
