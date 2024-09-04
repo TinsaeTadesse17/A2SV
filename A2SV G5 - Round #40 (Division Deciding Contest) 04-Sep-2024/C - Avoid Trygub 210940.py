d28d4d0aa7add52c2b7f90ae6d3b89a64c63d6ac
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-09-04 11:35:11 +0300

    Created 'C - Avoid Trygub' by A2SV Hub

---
+++ A2SV G5 - Round #40 (Division Deciding Contest) 04-Sep-2024/C - Avoid Trygub 210940.py
@@ -0,0 +1,8 @@
+# Problem: C - Avoid Trygub - https://codeforces.com/gym/543431/problem/C
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    string =  input()
+    
+    print(''.join(sorted(string)))
