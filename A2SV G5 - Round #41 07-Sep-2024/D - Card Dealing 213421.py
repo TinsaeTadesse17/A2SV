c6694d0926bee0887813340eb82c0645cbb40b29
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-09-07 22:47:09 +0300

    Created 'D - Card Dealing' by A2SV Hub

---
+++ A2SV G5 - Round #41 07-Sep-2024/D - Card Dealing 213421.py
@@ -0,0 +1,30 @@
+# Problem: D - Card Dealing - https://codeforces.com/gym/545013/problem/D
+
+t = int(input())
+
+for _ in range(t):
+    cards = int(input()) - 1
+
+    alice = 1
+    bob = 0
+    card = 2
+
+    while cards > 0:
+        if cards >= 2 * card + 1:
+            bob += 2 * card + 1
+            cards -= 2 * card + 1
+        else:
+            bob += cards
+            cards = 0
+
+        if cards == 0:
+            break
+        card += 2
+        if cards >= 2 * card + 1:
+            alice += 2 * card + 1
+            cards -= 2 * card + 1
+            card += 2
+        else:
+            alice += cards 
+            cards = 0
+    print(alice,bob)
