dbb25c8ff51d0faf90b7fb7897e1d4e16a4de598
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-09-07 22:47:23 +0300

    Created 'E - Min Divisible Outside Segment' by A2SV Hub

---
+++ A2SV G5 - Round #41 07-Sep-2024/E - Min Divisible Outside Segment 213422.py
@@ -0,0 +1,15 @@
+# Problem: E - Min Divisible Outside Segment - https://codeforces.com/gym/545013/problem/E
+
+t = int(input())
+
+for _ in range(t):
+
+    l , r , d = map(int,input().split())
+    if l <= d <= r:
+        ans = ((r // d) + 1) * d
+    elif d < l:
+        ans = d
+    else:
+        ans = d
+
+    print(ans)
