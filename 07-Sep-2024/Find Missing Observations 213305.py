28c5aa8ef195319b50b2f036a4659fe52858c14c
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-09-07 22:43:50 +0300

    Created 'Find Missing Observations' by A2SV Hub

---
+++ 07-Sep-2024/Find Missing Observations 213305.py
@@ -0,0 +1,19 @@
+# Problem: Find Missing Observations - https://leetcode.com/problems/find-missing-observations/
+
+class Solution:
+    def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
+        m = len(rolls)
+        
+        missing_sum = (mean * (n + m)) - sum(rolls)
+        # Check if it's possible to construct a valid sequence
+        if missing_sum < n or missing_sum > 6 * n:
+            return []
+        
+        result = []
+        while missing_sum > 0:
+            dice = min(missing_sum - n + 1, 6)  
+            result.append(dice)
+            missing_sum -= dice
+            n -= 1
+        
+        return result
