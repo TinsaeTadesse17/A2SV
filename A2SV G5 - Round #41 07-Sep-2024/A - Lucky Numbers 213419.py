20205dec7674cf1cc371e254e96eeb94e13fd1ba
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-09-07 22:46:48 +0300

    Created 'A - Lucky Numbers' by A2SV Hub

---
+++ A2SV G5 - Round #41 07-Sep-2024/A - Lucky Numbers 213419.py
@@ -0,0 +1,16 @@
+# Problem: A - Lucky Numbers - https://codeforces.com/gym/545013/problem/A
+
+n,k = map(int, input().split())
+arr = list(map(int, input().split()))
+count = 0
+ans = 0
+
+for i in range(n):
+    for j in str(arr[i]):
+        if j == "4" or j == "7":
+            count += 1
+    if count <= k:
+        ans += 1
+    count = 0
+
+print(ans)
