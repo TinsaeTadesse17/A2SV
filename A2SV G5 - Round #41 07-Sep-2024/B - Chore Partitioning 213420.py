c7698dcddc6613d7c283daf46c3ace3d69b9f9bc
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-09-07 22:46:59 +0300

    Created 'B - Chore Partitioning' by A2SV Hub

---
+++ A2SV G5 - Round #41 07-Sep-2024/B - Chore Partitioning 213420.py
@@ -0,0 +1,11 @@
+# Problem: B - Chore Partitioning - https://codeforces.com/gym/545013/problem/B
+
+n , a , b = map(int, input().split())
+complexity = list(map(int, input().split()))
+
+complexity.sort()
+ans = complexity[-a] - complexity[b-1]
+if  ans > 0:
+    print(ans)
+else:
+    print(0)
