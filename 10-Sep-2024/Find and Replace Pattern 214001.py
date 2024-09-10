14b8148ad330d86108bf07e43130165455f5a420
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-09-10 10:57:33 +0300

    Created 'Find and Replace Pattern' by A2SV Hub

---
+++ 10-Sep-2024/Find and Replace Pattern 214001.py
@@ -0,0 +1,25 @@
+# Problem: Find and Replace Pattern - https://leetcode.com/problems/find-and-replace-pattern/
+
+class Solution:
+    def findAndReplacePattern(self, words: List[str], pattern: str) -> List[str]:
+        ans = []
+        for word in words:
+            hashmap = {}
+            hashmap2 = {}
+            flag = True
+            for idx,char in enumerate(word):
+                if char in hashmap:
+                    if hashmap[char] != pattern[idx]:
+                        flag = False
+                        break
+                elif pattern[idx] in hashmap2:
+                    if hashmap2[pattern[idx]] != char:
+                        flag = False
+                        break
+                else:
+                    hashmap[char] = pattern[idx]
+                    hashmap2[pattern[idx]] = char
+            if flag:
+                ans.append(word)
+        return ans
+
