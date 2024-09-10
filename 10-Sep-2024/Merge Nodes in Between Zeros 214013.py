605194701e8d8e7b058137e136e3e6b2149e0f2c
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-09-10 11:55:03 +0300

    Created 'Merge Nodes in Between Zeros' by A2SV Hub

---
+++ 10-Sep-2024/Merge Nodes in Between Zeros 214013.py
@@ -0,0 +1,25 @@
+# Problem: Merge Nodes in Between Zeros - https://leetcode.com/problems/merge-nodes-in-between-zeros
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(-1)
+        new_head = dummy
+        head = head.next
+        curr_sum = 0
+
+        while head:
+            if head.val != 0:
+                curr_sum += head.val
+            else:
+                new_node = ListNode(curr_sum)
+                new_head.next = new_node
+                new_head = new_head.next
+                curr_sum = 0
+            head = head.next
+
+        return dummy.next
