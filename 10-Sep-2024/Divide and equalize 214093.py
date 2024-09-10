d30cfdf82b77c6db7c15a1f8b8fe9d904151a594
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-09-10 17:20:11 +0300

    Created 'Divide and equalize' by A2SV Hub

---
+++ 10-Sep-2024/Divide and equalize 214093.py
@@ -0,0 +1,34 @@
+# Problem: Divide and equalize - https://codeforces.com/problemset/problem/1881/D
+
+from collections import Counter
+t = int(input())
+results = []
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+
+    factorization = []
+    for num in arr:
+        d = 2
+        while d * d <= num:
+            while num % d == 0:
+                factorization.append(d)
+                num //= d
+            d += 1
+        if num > 1:
+            factorization.append(num)
+
+    counter = Counter(factorization)
+    flag = True
+    for count in counter.values():
+        if count % len(arr) != 0:
+            print("NO")
+            flag = False
+            break
+    if flag:
+        print("YES")
+    
+       
+
+
+  
