70ff98654914793bc976b826b5d980eb6f909987
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-09-10 15:09:57 +0300

    Created 'Fedor and New Game' by A2SV Hub

---
+++ 10-Sep-2024/Fedor and New Game 214043.py
@@ -0,0 +1,14 @@
+# Problem: Fedor and New Game - https://codeforces.com/contest/467/problem/B
+
+n , m , k = map(int,input().split())
+players = []
+friends = 0
+for _ in range(m+1):
+    x = int(input())
+    players.append(x)
+for i in range(len(players)-2,-1,-1):
+    xor = players[-1] ^ players[i]
+    if bin(xor).count('1') <= k:
+        friends += 1
+print(friends)
+    
