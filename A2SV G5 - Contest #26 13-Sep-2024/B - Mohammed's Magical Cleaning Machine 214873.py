9f40e44051ab24de08242af9235c0911d5a4df79
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-09-13 16:39:47 +0300

    Created 'B - Mohammed's Magical Cleaning Machine' by A2SV Hub

---
+++ A2SV G5 - Contest #26 13-Sep-2024/B - Mohammed's Magical Cleaning Machine 214873.py
@@ -0,0 +1,19 @@
+# Problem: B - Mohammed's Magical Cleaning Machine - https://codeforces.com/gym/537362/problem/B
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int,input().split()))
+
+    count_zero = 0
+    dust_level = 0
+    flag = False
+
+    for i in range(len(arr)-1):
+        if arr[i] != 0:
+            dust_level += arr[i]
+            flag = True
+        if flag and arr[i] == 0:
+            count_zero += 1
+
+    print(count_zero + dust_level)
