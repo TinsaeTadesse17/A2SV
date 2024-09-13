ad104f31e59024f9db2e4433586a35049c0f4c7c
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-09-13 15:36:26 +0300

    Created 'B - BANless' by A2SV Hub

---
+++ A2SV G5 - Contest #20 13-Sep-2024/B - BANless 214866.py
@@ -0,0 +1,14 @@
+# Problem: B - BANless - https://codeforces.com/gym/528792/problem/B
+
+import math
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    print(math.ceil(n / 2 ))
+    start , end = 1 , 3 * n
+    while start < end:
+        print(start,end)
+        start += 3
+        end -= 3
+
+        
