212a5c483e41ff8286a8a6b75701f4c159178756
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-09-13 13:09:43 +0300

    Created 'B - Fasika and Children' by A2SV Hub

---
+++ A2SV G5 - Contest #27 13-Sep-2024/B - Fasika and Children 214814.py
@@ -0,0 +1,16 @@
+# Problem: B - Fasika and Children - https://codeforces.com/gym/538762/problem/B
+
+import math
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+max_round = -1
+ans = -1
+
+for idx, num in enumerate(arr):
+    rounds = math.ceil(num / m)
+    if rounds >= max_round:
+        max_round = rounds
+        ans = idx
+
+print(ans + 1)
