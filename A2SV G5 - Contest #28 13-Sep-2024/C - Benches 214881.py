e70fcf7d4955d931780d3ecf15e5c9bb34517e86
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-09-13 17:30:32 +0300

    Created 'C - Benches' by A2SV Hub

---
+++ A2SV G5 - Contest #28 13-Sep-2024/C - Benches 214881.py
@@ -0,0 +1,16 @@
+# Problem: C - Benches - https://codeforces.com/gym/540354/problem/C
+
+from heapq import heapify, heappop, heappush
+
+n = int(input())
+m = int(input())
+benches = []
+for _ in range(n):
+    benches.append(int(input()))
+max_people = max(benches) + m
+heapify(benches)
+for i in range(m):
+    min_bench = heappop(benches)
+    heappush(benches, min_bench + 1)
+
+print(max(benches),max_people)
