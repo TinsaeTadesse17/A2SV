9b0a39dc239c7f8343aa3b052cb58166e479e515
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-09-14 06:23:22 +0300

    Created 'A - Shalamagando's  workout' by A2SV Hub

---
+++ A2SV G5 - Contest #12 14-Sep-2024/A - Shalamagando's  workout 215018.py
@@ -0,0 +1,14 @@
+# Problem: A - Shalamagando's  workout - https://codeforces.com/gym/517685/problem/A
+
+n = int(input())
+arr = list(map(int, input().split()))
+ans = [0,0,0]
+for i in range(n):
+    ans[i%3] += arr[i]
+
+if ans[0] == max(ans):
+    print("chest")
+elif ans[1] == max(ans):
+    print("biceps")
+else:    
+    print("back")
