4ff2ca0ce31062f1f37ad725ffcf3b855d079e15
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-09-16 12:48:26 +0300

    Created 'Minimum Time Difference' by A2SV Hub

---
+++ 16-Sep-2024/Minimum Time Difference 215410.py
@@ -0,0 +1,21 @@
+# Problem: Minimum Time Difference - https://leetcode.com/problems/minimum-time-difference/
+
+class Solution:
+    def findMinDifference(self, timePoints: List[str]) -> int:
+        timePoints.sort()
+        
+        def converter(time):
+            hours , minutes = map(int,time.split(":"))
+            return 60 * hours + minutes
+
+        minutes = [converter(time) for time in timePoints]
+        #1440 is minutes in a day
+        #considering circular time diff b/n the first and the last time points  
+        ans = 1440 - converter(timePoints[-1]) + converter(timePoints[0])
+
+        for i in range(1,len(timePoints)):
+            diff = converter(timePoints[i]) - converter(timePoints[i-1])
+            ans = min(ans,diff)
+
+        return ans
+
