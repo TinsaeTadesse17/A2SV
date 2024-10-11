ab5571eeb645de69bea8821b3e8e420721c5b229
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-10-11 15:00:33 +0300

    Created 'Path with Maximum Probability' by A2SV Hub

---
+++ 11-Oct-2024/Path with Maximum Probability 219429.py
@@ -0,0 +1,34 @@
+# Problem: Path with Maximum Probability - https://leetcode.com/problems/path-with-maximum-probability/
+
+class Solution:
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
+        m = len(edges)
+        for i in range(m):
+            edges[i].append(succProb[i])
+        for i in range(m):      
+            edges.append([edges[i][1] , edges[i][0] , succProb[i]])
+
+
+        curr = [float("-inf") for _ in range(n)]
+        prev = [float("-inf") for _ in range(n)]
+        ans = 0
+
+        prev[start_node] = 1
+        curr[start_node] = 1
+
+        for i in range(n-1):
+            for u , v , w in edges:
+                curr[v] = max(prev[u] * w , curr[v])
+                if v == end_node:
+                    ans = max(ans,curr[v])
+
+            if prev == curr:
+                break
+            prev = curr.copy()
+
+        return ans 
+
+
+
+
+
