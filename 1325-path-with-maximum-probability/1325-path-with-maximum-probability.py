a8024d8c014e306a17b0c38657f4099cb159b871
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-10-11 15:35:16 +0300

    Time: 538 ms (75.31%), Space: 27.9 MB (77.93%) - LeetHub

--- 1325-path-with-maximum-probability/1325-path-with-maximum-probability.py
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.py
@@ -1,32 +1,24 @@
 class Solution:
     def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
         m = len(edges)
+        adj = defaultdict(list)
         for i in range(m):
-            edges[i].append(succProb[i])
-        for i in range(m):      
-            edges.append([edges[i][1] , edges[i][0] , succProb[i]])
+            adj[edges[i][0]].append((edges[i][1],succProb[i]))
+            adj[edges[i][1]].append((edges[i][0],succProb[i]))
+        probabilities = [float("-inf") for _ in range(n)]
+        probabilities[start_node] = 1
+        
+        queue = deque([(1,start_node)])
 
+        while queue:
+            prob , node = queue.popleft()
 
-        curr = [float("-inf") for _ in range(n)]
-        prev = [float("-inf") for _ in range(n)]
-        ans = 0
+            for neigh , succProb in adj[node]:
+                if probabilities[node] * succProb > probabilities[neigh]:
+                    probabilities[neigh] = probabilities[node] * succProb
+                    queue.append((probabilities[neigh], neigh))
 
-        prev[start_node] = 1
-        curr[start_node] = 1
-
-        for i in range(n-1):
-            for u , v , w in edges:
-                curr[v] = max(prev[u] * w , curr[v])
-                if v == end_node:
-                    ans = max(ans,curr[v])
-
-            if prev == curr:
-                break
-            prev = curr.copy()
-
-        return ans 
-
-
-
+        return probabilities[end_node] if probabilities[end_node] != float("-inf") else 0
+                
 
 
