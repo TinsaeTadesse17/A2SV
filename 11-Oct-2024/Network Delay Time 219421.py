23db9fc73027336cb50482aa79b042127bf3b4a8
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-10-11 14:23:04 +0300

    Created 'Network Delay Time' by A2SV Hub

---
+++ 11-Oct-2024/Network Delay Time 219421.py
@@ -0,0 +1,17 @@
+# Problem: Network Delay Time - https://leetcode.com/problems/network-delay-time/
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        curr = [float("inf") for _ in range(n)]
+        prev = [float("inf") for _ in range(n)]
+
+        prev[k-1] = 0
+        curr[k-1] = 0
+
+        for i in range(n-1):
+            for u , v, w in times:
+                curr[v-1] = min(prev[u-1] + w , curr[v-1])
+
+            prev = curr.copy()
+
+        return max(curr) if max(curr) != float("inf") else -1
