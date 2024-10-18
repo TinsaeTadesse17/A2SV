cbec5e29a32f6de4a31814a0b1e3e233c2d6f3a4
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-10-18 10:44:52 +0300

    Time: 0 ms (100%), Space: 16.7 MB (10.93%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.py
+++ 0670-maximum-swap/0670-maximum-swap.py
@@ -1,13 +1,25 @@
 class Solution:
-  def maximumSwap(self, num: int) -> int:
-    arr = list(str(num))
-    
-    last = {int(d): i for i, d in enumerate(arr)}
-    
-    for i, digit in enumerate(arr): # Changed 'num' to 'digit' 
-      for d in range(9, int(digit), -1): 
-        if last.get(d, -1) > i:
-          arr[i], arr[last[d]] = arr[last[d]], arr[i]
-          return int(''.join(arr))
-    
-    return int(num)
+    def maximumSwap(self, num: int) -> int:
+        arr = list(str(num))
+        for i, a in enumerate(arr):
+            arr[i] = int(a)
+
+        arr.sort(reverse=True)
+        num = list(str(num))
+        n = len(num)
+
+        swap = float("inf")
+        swap_idx = -1
+
+        for idx, digit in enumerate(num):
+            if digit != str(arr[idx]):
+                swap = str(arr[idx])
+                swap_idx = idx
+                break
+
+        if swap != float("inf"):
+            for i in range(n-1, -1, -1):
+                if num[i] == swap:
+                    num[i] , num[swap_idx] = num[swap_idx] , num[i]
+
+        return int(''.join(num))
