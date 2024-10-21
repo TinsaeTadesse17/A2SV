9bdb58962bd1a11a7a7eaf968f45313f364e3d26
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-10-21 03:22:38 +0300

    Created 'A - Se7en' by A2SV Hub

---
+++ A2SV G5 - Contest #26 21-Oct-2024/A - Se7en 220908.py
@@ -0,0 +1,18 @@
+# Problem: A - Se7en - https://codeforces.com/gym/537362/problem/A
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    flag = True
+    if n % 7 == 0:
+        print(n)
+        flag = False
+    if flag:
+       
+
+        for i in range(10):
+            num = str(n)[:-1]
+            num = num + str(i)
+            if int(num )% 7 == 0:
+                print(int(num))
+                break
