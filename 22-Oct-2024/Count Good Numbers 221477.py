6cb40e5d0cc432b717c5297f99c2fa71a18e8e31
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-10-22 18:36:46 +0300

    Created 'Count Good Numbers' by A2SV Hub

---
+++ 22-Oct-2024/Count Good Numbers 221477.py
@@ -0,0 +1,18 @@
+# Problem: Count Good Numbers - https://leetcode.com/problems/count-good-numbers/
+
+class Solution:
+    def countGoodNumbers(self, n: int) -> int:
+        m = 10 ** 9 + 7
+        def binary_expo(a,b):
+            if b == 0:
+                return 1
+
+            if b % 2 == 0:
+                return binary_expo((a * a) % m , b//2)
+            else:
+                return a * binary_expo((a * a) % m , (b-1) // 2)
+
+        if n % 2:
+            return (binary_expo(5,ceil(n/2)) % m * binary_expo(4,n//2) % m) % m
+        else:
+            return (binary_expo(5,n/2) % m * binary_expo(4,n/2) % m) % m
