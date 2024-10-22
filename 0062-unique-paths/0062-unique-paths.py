d57cfe7033c34563af1fc37be35088c36502ec3a
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-10-22 18:36:32 +0300

    Time: 0 ms (100%), Space: 16.7 MB (11.71%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,7 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        right = n - 1
+        down = m - 1
+        total = right + down
+
+        return factorial(total) // (factorial(right) * factorial(down))
