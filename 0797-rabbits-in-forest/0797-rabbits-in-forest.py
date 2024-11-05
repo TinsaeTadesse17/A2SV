312c9fd4a552c92c0d5315b2caf0e219de1db813
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-11-05 11:59:41 +0300

    Time: 0 ms (100%), Space: 16.8 MB (14.2%) - LeetHub

--- 0797-rabbits-in-forest/0797-rabbits-in-forest.py
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.py
@@ -3,12 +3,6 @@ class Solution:
         counter = Counter(answers)
         ans = 0
         for val,count in counter.items():
-            if val == 0:
-                ans += count
-            elif count <= val + 1:
-                ans += val + 1
-            else:
-                ans += ceil(count / (val+1)) * (val+1)
-
+            ans += ceil(count / (val+1)) * (val+1)
         return ans
         
