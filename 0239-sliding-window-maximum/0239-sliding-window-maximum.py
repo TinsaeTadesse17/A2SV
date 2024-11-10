aef6e7851d651794eb8114aaae57a78c464c3a65
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-11-11 00:57:05 +0300

    Time: 187 ms (33.45%), Space: 30.7 MB (77.49%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,20 +1,20 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         n = len(nums)
-        
-        deq = deque()
-        max_vals = []
+
+        queue = deque()
+        ans = []
 
         for i in range(n):
-            if deq and deq[0] == i - k:
-                deq.popleft()
-            
-            while deq and nums[deq[-1]] < nums[i]:
-                deq.pop()
-            
-            deq.append(i)
-            
-            if i >= k - 1:
-                max_vals.append(nums[deq[0]])
+            if queue and queue[0] == i-k:
+                queue.popleft()
 
-        return max_vals
+            while queue and nums[queue[-1]] <= nums[i]:
+                queue.pop()
+
+            queue.append(i)
+
+            if i >= k-1:
+                ans.append(nums[queue[0]])
+
+        return ans
