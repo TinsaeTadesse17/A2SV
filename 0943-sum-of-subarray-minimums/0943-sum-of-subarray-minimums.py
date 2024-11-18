271b34a2a70025c46c936a9dbcc8531960c0f525
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-11-18 13:40:49 +0300

    Time: 100 ms (44.98%), Space: 20.6 MB (83.61%) - LeetHub

---
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.py
@@ -0,0 +1,31 @@
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        n = len(arr)
+        left_monstack = []
+        left = [-1] * n
+        for i in range(n):
+            while left_monstack and arr[i] < arr[left_monstack[-1]]:
+                left_monstack.pop()
+            if left_monstack:
+                left[i] = left_monstack[-1]
+            else:
+                left[i] = -1
+            left_monstack.append(i)
+            
+
+        right_monstack = []
+        right = [n] * n
+        for i in range(n-1,-1,-1):
+            while right_monstack  and arr[i] <= arr[right_monstack [-1]]:
+                right_monstack.pop()
+            if right_monstack:
+                right[i] = right_monstack[-1]
+            else:
+                right[i] = n
+            right_monstack.append(i)
+
+        ans = 0
+        for i in range(n):
+            ans += ((i - left[i]) * (right[i] - i)) * arr[i]
+
+        return ans % (10**9 + 7)
