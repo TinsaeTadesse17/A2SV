ef098a23800a86686fe76f2511257a0f1739534f
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-11-19 06:00:48 +0300

    Time: 178 ms (85.91%), Space: 33.2 MB (35.83%) - LeetHub

--- 0892-shortest-subarray-with-sum-at-least-k/0892-shortest-subarray-with-sum-at-least-k.py
+++ 0892-shortest-subarray-with-sum-at-least-k/0892-shortest-subarray-with-sum-at-least-k.py
@@ -1,25 +1,49 @@
 class Solution:
     def shortestSubarray(self, nums: List[int], k: int) -> int:
         n = len(nums)
-        prefix_sum = [0] * (n + 1)
-        
-        # prefix sums
+
+        prefix_sum = [0] * (n+1)
         for i in range(n):
-            prefix_sum[i + 1] = prefix_sum[i] + nums[i]
+            prefix_sum[i+1] = prefix_sum[i] + nums[i]
+
+        min_subarray = float("inf")
+        prefix_indices = deque() #monotonic so that smallest prefix sum is always at the front
+
+        for i,summ in enumerate(prefix_sum):
+            while prefix_indices and summ - prefix_sum[prefix_indices[0]] >= k: #valid prefix sum, minimize length
+                min_subarray = min(min_subarray,i-prefix_indices.popleft())
+
+            while prefix_indices and summ <= prefix_sum[prefix_indices[-1]]: #smaller prefix sums correspond to earlier indices, and subtracting them minimizes the subarray length so pop until current prefix sum is the largest
+                prefix_indices.pop()
+
+            prefix_indices.append(i)
+
+        return min_subarray if min_subarray != float('inf') else -1
+
+
+
+
+
+
+
+
+
+
+
         
-        # indices of the prefix sums
-        indices = deque()
-        ans = float('inf')
         
-        for i in range(n + 1):
-            # valid?
-            while indices and prefix_sum[i] - prefix_sum[indices[0]] >= k:
-                ans = min(ans, i - indices.popleft())
+
             
-            # increasing prefix sums
-            while indices and prefix_sum[i] <= prefix_sum[indices[-1]]:
-                indices.pop()
-            
-            indices.append(i)
+
+
+
+
+
+
+
+
+
+
+
         
-        return ans if ans != float('inf') else -1
+        
