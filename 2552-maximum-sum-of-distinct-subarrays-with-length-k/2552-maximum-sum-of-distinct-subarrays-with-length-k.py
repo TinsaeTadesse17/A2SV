511cd3d81ae9167f68d48c118dc08387540e7692
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-11-19 09:47:41 +0300

    Time: 247 ms (5.05%), Space: 36.2 MB (38.35%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -8,15 +8,18 @@ class Solution:
         max_sum = 0
         counter = Counter()
         for i,num in enumerate(nums):
+            #add element
             counter[num] += 1
             curr_sum  += num
 
+            #remove element and deduct sum when window size exceeds
             if i >= k:
                 counter[nums[i-k]] -= 1
                 if counter[nums[i-k]] == 0:
                     del counter[nums[i-k]]
                 curr_sum -= nums[i-k]
 
+            #valid window size
             if len(counter) == k:
                 max_sum = max(max_sum , curr_sum)            
         return max_sum
