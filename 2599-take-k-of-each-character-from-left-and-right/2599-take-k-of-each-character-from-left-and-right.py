fa9c3acdbae8f6fad2f506cf8a74fe3fb2f387ba
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-11-21 18:12:07 +0300

    Time: 239 ms (47.34%), Space: 17.1 MB (95.64%) - LeetHub

--- 2599-take-k-of-each-character-from-left-and-right/2599-take-k-of-each-character-from-left-and-right.py
+++ 2599-take-k-of-each-character-from-left-and-right/2599-take-k-of-each-character-from-left-and-right.py
@@ -1,16 +1,20 @@
 class Solution:
     def takeCharacters(self, s: str, k: int) -> int:
         if k == 0:
-            return 0
-        tot_count = Counter(s)
-        if len(tot_count) < 3 or any(count < k for count in tot_count.values()):
-            return -1
-        extra_count = defaultdict(int)
-        window_width, left = 0, 0
-        for right, ch in enumerate(s):
-            extra_count[ch] += 1
-            while tot_count[ch] - extra_count[ch] < k:
-                extra_count[s[left]] -= 1
+            return k
+        tot_counter = Counter(s)
+        if len(tot_counter) < 3 or any(cnt < k for cnt in tot_counter.values()):
+            return -1 
+
+        window_counter = Counter()
+        left = 0
+        window_size = 0
+        for right,char in enumerate(s):
+            window_counter[char] += 1
+
+            while tot_counter[char] - window_counter[char] < k:
+                window_counter[s[left]] -= 1
                 left += 1
-            window_width = max(right - left + 1, window_width)
-        return len(s) - window_width
+            window_size = max(window_size,right-left+1)
+        return len(s) - window_size
+
