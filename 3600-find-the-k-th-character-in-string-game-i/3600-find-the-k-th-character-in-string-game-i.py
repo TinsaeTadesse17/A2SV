22b7c494fcb1948245a352a786826e20d90aac54
Tinsae Tadesse <74698062+TinsaeTadesse17@users.noreply.github.com>
2024-12-03 11:45:20 +0300

    Time: 0 ms (100%), Space: 17.3 MB (5.09%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.py
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.py
@@ -2,7 +2,10 @@ class Solution:
     def kthCharacter(self, k: int) -> str:
         def next(char):
             return chr((ord(char) - 95 ) % 26 + 96)
-        next_count = 0
+        next_count = 0 
+        '''stores the number of times k was found on the second half, 
+        means the character is a result of transformation so we can count
+        this to know how many times we need to transform to get the answer'''
         size = 1
         while size < k :
             size *= 2
